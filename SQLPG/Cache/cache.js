c7089c4f92784faf256ca091dac448aa632be6b9
vlieu425 <121201991+vlieu425@users.noreply.github.com>
2024-03-27 16:55:12 -0700

    implement nodecache

---
+++ SQLPG/Cache/cache.js
@@ -0,0 +1,17 @@
+const NodeCache = require('node-cache');
+
+const cache = new NodeCache();
+
+function cacheMiddleware(req, res, next) {
+  const key = req.originalUrl;
+  const cachedData = cache.get(key);
+
+  if (cachedData) {
+    console.log('Data found in cache');
+    return res.json(cachedData);
+  }
+
+  next();
+}
+
+module.exports = { cache, cacheMiddleware };
--- SQLPG/server/router.js
+++ SQLPG/server/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const controller = require('./controllers.js');
 const router = express.Router();
+const { cacheMiddleware } = require("../Cache/cache.js");
 
-router.get('/reviews/:productId', controller.getAllReviews);
-router.get('/reviews/meta/:productId', controller.getMetaData);
+
+router.get('/reviews/:productId', cacheMiddleware, controller.getAllReviews);
+router.get('/reviews/meta/:productId', cacheMiddleware, controller.getMetaData);
 router.put('/reviews/helpful/:reviewId', controller.updateHelpfulCount);
 router.put('/reviews/report/:reviewId', controller.reportReview);
 
